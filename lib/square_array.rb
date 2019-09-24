1088face600d2f911010f6e6333cd665b3fbf563
Thomas Wright <twwright@gmail.com>
2019-09-24 06:10:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   counter = 0
   while array[counter] do
-    squared = array[counter]*array[counter]
-    puts squared
+    puts array.shift**2
     counter += 1
   end
 end
